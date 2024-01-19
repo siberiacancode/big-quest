ace5942f80de60d747d000ad616ff70368e017d5
Daniil Khakhulin <dan.praeco@gmail.com>
2024-01-20 01:13:48 +0700

    :ocean: added test DataTableDemo component

---
+++ app/organizations/page.tsx
@@ -0,0 +1,12 @@
+import { DataTableDemo } from '@/components/common/Table/Table';
+
+const OrganizationsPage = () => {
+  return (
+    <>
+      <span>Organizations Page</span>
+      <DataTableDemo />
+    </>
+  );
+};
+
+export default OrganizationsPage;
