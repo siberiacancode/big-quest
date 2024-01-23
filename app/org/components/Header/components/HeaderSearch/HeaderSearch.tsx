c15aea0a74564d4d8657329fb276ce8f83f269ef
Volkov Dmitriy <vdy0555@gmail.com>
2024-01-23 11:13:49 +0700

    feature/BQ-182 🚀 add icon in shadcn input

--- app/org/components/Header/components/HeaderSearch/HeaderSearch.tsx
+++ app/org/components/Header/components/HeaderSearch/HeaderSearch.tsx
@@ -3,11 +3,10 @@ import { SearchIcon } from 'lucide-react';
 import { Input } from '@/components/ui';
 
 export const HeaderSearch = () => (
-  <div className='relative'>
-    <Input className='bg-secondary pl-10' placeholder='Найти...' type='search' />
-    <SearchIcon
-      className='pointer-events-none absolute left-2.5 top-1/2 -translate-y-1/2 opacity-50'
-      size={24}
-    />
-  </div>
+  <Input
+    className='bg-secondary'
+    placeholder='Найти...'
+    type='search'
+    icon={<SearchIcon className='opacity-50' />}
+  />
 );
