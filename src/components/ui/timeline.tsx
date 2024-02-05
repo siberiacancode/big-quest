2ca49e8dd5b8ea5568dd4bc8b8b62e3d8cb7fd07
Polina <polina.alina.2015@mail.ru>
2024-02-05 23:13:05 +0700

    fix/BQ-195 🐢 remove timeline position absolute, change types

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -1,23 +1,22 @@
-import type { ReactNode } from 'react';
 import React from 'react';
 
 import { cn } from '@/lib/utils';
 
-type TimelineContentProps = {
-  children: ReactNode;
-};
-
-const TimelineContent: React.FC<TimelineContentProps> = ({ children }) => (
-  <div className={cn(' text-sm	font-normal	text-slate-500	')}>{children}</div>
+const TimelineContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ children, className, ...props }, ref) => (
+    <div ref={ref} className={cn('mb-5 text-sm	font-normal text-slate-500', className)} {...props}>
+      {children}
+    </div>
+  )
 );
 TimelineContent.displayName = 'TimelineContent';
 
-type TimelineTitleProps = {
-  children: ReactNode;
-};
-
-const TimelineTitle: React.FC<TimelineTitleProps> = ({ children }) => (
-  <div className={cn('text-lg font-bold')}>{children}</div>
+const TimelineTitle = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ children, className, ...props }, ref) => (
+    <div ref={ref} className={cn('text-lg font-bold', className)} {...props}>
+      {children}
+    </div>
+  )
 );
 TimelineTitle.displayName = 'TimelineTitle';
 
@@ -26,43 +25,37 @@ const TimelineDot: React.FC = () => (
 );
 TimelineDot.displayName = 'TimelineDot';
 
-type TimelineItemProps = {
-  children: React.ReactNode;
-  className?: string;
-};
-
-const TimelineItem: React.FC<TimelineItemProps> = ({ children, className }) => (
-  <div className={cn('relative flex', className)}>
-    <TimelineDot />
-    <div className='absolute left-8 top-[-7px] flex flex-col gap-2 pr-9'>{children}</div>
-  </div>
-);
-TimelineItem.displayName = 'TimelineItem';
-
 const TimelineConnect: React.FC = () => (
-  <div className={cn('top-3 ml-[5px]  h-32 w-[1px] self-stretch bg-gray-400')} />
+  <div className={cn('ml-[5px] h-full min-h-32 w-[1px] bg-gray-400')} />
 );
 TimelineConnect.displayName = 'TimelineConnect';
 
-type TimelineProps = {
-  children: React.ReactNode;
-};
-
-const Timeline: React.FC<TimelineProps> = ({ children }) => {
-  const timelineItems = React.Children.toArray(children);
-
-  return (
-    <div className={cn('relative my-6 flex flex-col')}>
-      {timelineItems.map((child, index) => (
-        <React.Fragment key={index}>
-          {index > 0 && <TimelineConnect />}
-          {child}
-          {index === timelineItems.length - 1 && <TimelineConnect />}
-        </React.Fragment>
-      ))}
+const TimelineItem = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ children, className, ...props }, ref) => (
+    <div ref={ref} className={cn('flex gap-5', className)} {...props}>
+      <div>
+        <TimelineDot />
+        <TimelineConnect />
+      </div>
+      <div className='flex flex-col gap-2 pr-9'>{children}</div>
     </div>
-  );
-};
+  )
+);
+TimelineItem.displayName = 'TimelineItem';
+
+const Timeline = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ children, className, ...props }, ref) => {
+    const timelineItems = React.Children.toArray(children);
+
+    return (
+      <div ref={ref} className={cn('my-6 flex flex-col', className)} {...props}>
+        {timelineItems.map((child, index) => (
+          <React.Fragment key={index}>{child}</React.Fragment>
+        ))}
+      </div>
+    );
+  }
+);
 Timeline.displayName = 'Timeline';
 
 export { Timeline, TimelineContent, TimelineItem, TimelineTitle };
