8ee457aa78dc2e620887ce1e0a8713e2e0a3790a
Polina <polina.alina.2015@mail.ru>
2024-02-16 17:05:46 +0700

    fix/BQ-215 🐢 fix organizations page info card skeleton style

--- app/org/organizations/loading.tsx
+++ app/org/organizations/loading.tsx
@@ -3,24 +3,26 @@ import { Skeleton } from '@/components/ui/skeleton';
 const OrganizationsPageLoading = () => (
   <div className='bg-secondary px-4'>
     <div className='mb-4 h-6 w-[170px] rounded-lg' />
-    <div className='flex flex-wrap gap-3'>
-      <div className='flex flex-auto flex-col gap-2 rounded-lg bg-background p-6 shadow-sm'>
-        <div className='mb-1 flex justify-between'>
-          <Skeleton className='h-8 w-3/5 rounded-lg' />
-          <Skeleton className='h-9 w-9 rounded-full' />
+    <div className='flex flex-wrap gap-2 2smx:flex-col'>
+      <div className='flex w-6/12 flex-wrap gap-2 2smx:w-full 2smx:flex-row'>
+        <div className='flex flex-auto flex-col gap-2 rounded-lg bg-background p-6 shadow-sm'>
+          <div className='mb-1 flex justify-between'>
+            <Skeleton className='h-8 w-3/5 rounded-lg' />
+            <Skeleton className='h-9 w-9 rounded-full' />
+          </div>
+          <Skeleton className='h-9 w-1/3 rounded-lg' />
+          <Skeleton className='h-4 w-1/2 rounded-lg' />
         </div>
-        <Skeleton className='h-9 w-1/3 rounded-lg' />
-        <Skeleton className='h-4 w-1/2 rounded-lg' />
-      </div>
-      <div className='flex flex-auto flex-col gap-2 rounded-lg bg-background p-6 shadow-sm'>
-        <div className='mb-1 flex justify-between'>
-          <Skeleton className='h-8 w-3/5 rounded-lg' />
-          <Skeleton className='h-9 w-9 rounded-full' />
+        <div className='flex flex-auto flex-col gap-2 rounded-lg bg-background p-6 shadow-sm'>
+          <div className='mb-1 flex justify-between'>
+            <Skeleton className='h-8 w-3/5 rounded-lg' />
+            <Skeleton className='h-9 w-9 rounded-full' />
+          </div>
+          <Skeleton className='h-9 w-1/3 rounded-lg' />
+          <Skeleton className='h-4 w-1/2 rounded-lg' />
         </div>
-        <Skeleton className='h-9 w-1/3 rounded-lg' />
-        <Skeleton className='h-4 w-1/2 rounded-lg' />
       </div>
-      <div className='flex w-6/12 flex-col gap-2 rounded-lg bg-background p-6 shadow-sm'>
+      <div className='flex w-5/12 flex-auto flex-col gap-2 rounded-lg bg-background p-6 shadow-sm 2smx:w-full'>
         <div className='mb-1 flex justify-between'>
           <Skeleton className='h-8 w-2/5 rounded-lg' />
           <Skeleton className='h-9 w-9 rounded-full' />
