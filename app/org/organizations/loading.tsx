48ad983511b528ba31c26248e3adba0dba94ca7c
Polina <polina.alina.2015@mail.ru>
2024-02-16 14:40:09 +0700

    feature/BQ-215 🐢 add data table skeletons

--- app/org/organizations/loading.tsx
+++ app/org/organizations/loading.tsx
@@ -41,6 +41,72 @@ const OrganizationsPageLoading = () => (
         </div>
       </div>
     </div>
+    <div className='mt-10 w-full rounded-md bg-background p-4'>
+      <div className='flex w-full flex-wrap py-4 md:flex-nowrap'>
+        <div className='flex w-2/3 items-center mdx:flex-wrap'>
+          <Skeleton className='h-9 w-2/6 max-w-sm rounded-lg' />
+          <Skeleton className='m-2 h-9 w-1/6 rounded-lg' />
+          <Skeleton className='m-2 h-9 w-2/6 rounded-lg' />
+        </div>
+        <Skeleton className='mx-2 h-9 w-[116px] rounded-lg md:ml-auto' />
+      </div>
+      <div className='w-full rounded-md '>
+        <div className='w-full caption-bottom rounded-lg border border-secondary'>
+          <div className='flex h-12 border-b border-secondary bg-secondary' />
+          <div className='flex flex-col border-secondary [&_div:last-child]:border-0'>
+            {Array(10)
+              .fill({})
+              .map((_, index) => (
+                <div
+                  key={index}
+                  className='flex border-b border-secondary [&_div:last-child]:border-0'
+                >
+                  <div className='flex items-center p-4 pr-0 align-middle'>
+                    <Skeleton className='h-4 w-4 rounded-sm align-middle' />
+                  </div>
+                  <div className='flex w-36 items-center p-4 align-middle'>
+                    <Skeleton className='mx-4 h-4 w-full' />
+                  </div>
+                  <div className='flex w-56 items-center p-4 align-middle'>
+                    <Skeleton className='mx-4 h-4 w-full' />
+                  </div>
+                  <div className='flex w-36 items-center p-4 align-middle'>
+                    <Skeleton className='mx-4 h-4 w-full' />
+                  </div>
+                  <div className='flex w-32 items-center p-4 align-middle'>
+                    <Skeleton className='mx-4 h-4 w-full' />
+                  </div>
+                  <div className='flex w-44 items-center p-4 align-middle'>
+                    <Skeleton className='mx-4 h-4 w-full' />
+                  </div>
+                  <div className='flex w-36 items-center p-4 align-middle'>
+                    <Skeleton className='mx-4 h-4 w-full' />
+                  </div>
+                  <div className=' w-8 p-4 align-middle'>
+                    <Skeleton className='h-8 w-8 rounded-sm align-middle' />
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+      <div className='flex w-full items-center justify-between mdx:flex-col'>
+        <div className='mdx:pt-2'>
+          <Skeleton className='h-5 w-28' />
+        </div>
+        <div className='flex flex-col-reverse items-center justify-end gap-2 space-x-2 py-3 md:flex-row'>
+          <Skeleton className='h-10 w-1/3' />
+          <div className='flex items-center gap-2'>
+            <Skeleton className='h-5 w-32' />
+            <Skeleton className='h-8 w-12 px-3' />
+            <Skeleton className='h-8 w-8' />
+            <Skeleton className='h-8 w-8' />
+            <Skeleton className='h-8 w-8 rounded-lg' />
+            <Skeleton className='h-8 w-12 px-3' />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
