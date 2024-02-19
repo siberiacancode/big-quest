2c391143ace40658e6494c4dd1b30574a8e3726f
Polina <polina.alina.2015@mail.ru>
2024-02-20 01:04:09 +0700

    feature/BQ-220 🐢 add activities page and card

---
+++ app/org/organizations/[organizationId]/activities/components/ActivityCard/ActivityCard.tsx
@@ -0,0 +1,60 @@
+import { Clock5Icon, Edit3Icon, HeartIcon, UserRoundIcon, UsersRoundIcon } from 'lucide-react';
+import Image from 'next/image';
+
+import background from '@/assets/images/background/activity.png';
+import { Button, Separator, Typography } from '@/components/ui';
+
+export const ActivityCard = () => {
+  return (
+    <div className='w-72 rounded-lg bg-background p-4 2mdx:w-full xsx:w-full'>
+      <div className='relative'>
+        <Image className='h-64 w-full rounded-lg' src={background} alt='org-background' />
+        <div className='absolute top-0 flex w-full items-center justify-between p-3'>
+          <div className='rounded-md bg-secondary px-3 py-2'>
+            <Typography variant='sub4' tag='p'>
+              Опубликован
+            </Typography>
+          </div>
+          <Button variant='secondary' size='sm' className='rounded-full px-[10px] py-2'>
+            <Edit3Icon className='h-4 w-4' />
+          </Button>
+        </div>
+      </div>
+      <div className='mb-2 mt-3 flex justify-between text-muted-foreground'>
+        <Typography variant='sub4' tag='p' className='text-muted-foreground'>
+          Образование
+        </Typography>
+        <div className='flex gap-4'>
+          <div className='flex items-center gap-2'>
+            <UsersRoundIcon className='h-4 w-4 text-muted-foreground' />
+            <Typography variant='body4' tag='p'>
+              600
+            </Typography>
+          </div>
+          <div className='flex items-center gap-2'>
+            <HeartIcon className='h-4 w-4 text-muted-foreground' />
+            <Typography variant='body4' tag='p'>
+              210
+            </Typography>
+          </div>
+        </div>
+      </div>
+      <Typography variant='h7'>Рисуем живопись</Typography>
+      <Separator className='my-4' />
+      <div className='mb-1 flex justify-between'>
+        <div className='flex gap-2'>
+          <UserRoundIcon className='h-4 w-4 text-muted-foreground' />
+          <Typography variant='body3' tag='p' className='text-foreground'>
+            от 7+ лет
+          </Typography>
+        </div>
+        <div className='flex gap-2'>
+          <Clock5Icon className='h-4 w-4 text-muted-foreground' />
+          <Typography variant='body3' tag='p' className='text-foreground'>
+            2 ч
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/org/organizations/[organizationId]/activities/page.tsx
+++ app/org/organizations/[organizationId]/activities/page.tsx
@@ -1,3 +1,34 @@
-const OrganizationActivitiesPage = () => <div>Activities page</div>;
+import { PlusCircledIcon } from '@radix-ui/react-icons';
+
+import { I18nText } from '@/components/common';
+import { Button, Typography } from '@/components/ui';
+
+import { ActivityCard } from './components/ActivityCard/ActivityCard';
+
+const OrganizationActivitiesPage = () => (
+  <div>
+    <div className='flex justify-between'>
+      <Typography variant='h5' tag='h5'>
+        <I18nText path='partners.activities.title' />
+      </Typography>
+
+      <Button variant='light' className='mx-2 p-5' size='sm'>
+        <PlusCircledIcon className='mr-2 h-4 w-4' />
+        <I18nText path='button.addActivities' />
+      </Button>
+    </div>
+    <div className='flex w-full justify-center'>
+      <div className='mt-4 flex flex-wrap gap-7 xsx:flex xsx:grid-cols-none xsx:flex-col xsx:items-center'>
+        {Array(5)
+          .fill({})
+          .map((_, index) => (
+            <span key={index}>
+              <ActivityCard />
+            </span>
+          ))}
+      </div>
+    </div>
+  </div>
+);
 
 export default OrganizationActivitiesPage;
