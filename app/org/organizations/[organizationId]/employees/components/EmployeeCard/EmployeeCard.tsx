53e2bd8a04f4b5b22bd953e1262526d242dfa954
Daniil Khakhulin <dan.praeco@gmail.com>
2024-02-20 22:37:09 +0700

    feature/BQ-216 :ocean: employee card layout fix

--- app/org/organizations/[organizationId]/employees/components/EmployeeCard/EmployeeCard.tsx
+++ app/org/organizations/[organizationId]/employees/components/EmployeeCard/EmployeeCard.tsx
@@ -11,13 +11,13 @@ interface EmployeeCardProps {
 export const EmployeeCard = ({
   employee: { fullname, email, employeeRole, phoneNumber }
 }: EmployeeCardProps) => (
-  <Card className='relative min-w-[340px] flex-1'>
+  <Card className='relative min-w-[320px] flex-1'>
     <CardContent className='flex flex-row-reverse items-center justify-center p-8 pb-12 pr-5 pt-4'>
       <Edit3Icon className='right absolute right-4 top-4 text-placeholder' />
-      <div className='flex flex-1 flex-col items-center space-y-3'>
-        <div className='flex flex-col items-center'>
+      <div className='flex flex-1 flex-col items-center space-y-3 pt-4'>
+        <div className='mb-2 flex flex-col items-center'>
           <div className='h-16 w-16 rounded-full bg-slate-700' />
-          <div>
+          <div className='mt-2 flex flex-col items-center space-y-1'>
             <Typography variant='h4' tag='h3' className='flex-1'>
               {fullname}
             </Typography>
@@ -28,7 +28,7 @@ export const EmployeeCard = ({
         </div>
         <Separator />
         <div className='flex space-x-6'>
-          <div className='flex-1'>
+          <div className='flex-1 space-y-2'>
             <Typography variant='sub1' tag='p' className='flex-1'>
               <I18nText path='organization.employee.email' />
             </Typography>
@@ -36,9 +36,13 @@ export const EmployeeCard = ({
               <I18nText path='organization.employee.phoneNumber' />
             </Typography>
           </div>
-          <div className='flex-1'>
-            <div>{email}</div>
-            <div>{phoneNumber}</div>
+          <div className='flex-1 space-y-2'>
+            <Typography variant='sub1' tag='p' className='flex-1 font-normal'>
+              {email}
+            </Typography>
+            <Typography variant='sub1' tag='p' className='flex-1 font-normal'>
+              {phoneNumber}
+            </Typography>
           </div>
         </div>
       </div>
--- mock/organization/id/employees/get.ts
+++ mock/organization/id/employees/get.ts
@@ -8,63 +8,63 @@ export const getOrganizationEmployees: RestRequestConfig = {
       data: [
         {
           id: '909ddfab-9c32-4d78-88d2-366de0814b76',
-          fullname: 'Park',
+          fullname: 'Arthur Morgan',
           email: 'katinalang@bunga.com',
           employeeRole: 'Manager',
           phoneNumber: '(927) 424-3144'
         },
         {
           id: '59780ef9-391c-42ff-8141-080c2dc2db13',
-          fullname: 'Rivers',
+          fullname: 'Dutch van der Linde',
           email: 'letafoster@coash.com',
           employeeRole: 'Manager',
           phoneNumber: '(809) 521-3230'
         },
         {
           id: 'b92e8d44-0e12-4cb1-9d4a-7c4de50987a3',
-          fullname: 'Turner',
+          fullname: 'Sadie Adler',
           email: 'jimmiemathis@tasmania.com',
           employeeRole: 'Manager',
           phoneNumber: '(948) 476-3906'
         },
         {
           id: '769c3212-82ef-4da1-900d-3e69f7aa2ec6',
-          fullname: 'Hernandez',
+          fullname: 'John Marston',
           email: 'mccrayharding@kidgrease.com',
           employeeRole: 'Manager',
           phoneNumber: '(981) 522-3212'
         },
         {
           id: '72ffc9af-2185-44db-a962-ad749525505c',
-          fullname: 'Fuller',
+          fullname: 'Susan Grimshaw',
           email: 'maribelowens@liquicom.com',
           employeeRole: 'Manager',
           phoneNumber: '(929) 545-2962'
         },
         {
           id: '6280b394-7a6e-44b5-8d5e-a57f42901da6',
-          fullname: 'Barrera',
+          fullname: 'Javier Escuella',
           email: 'sylviacalderon@veraq.com',
           employeeRole: 'Manager',
           phoneNumber: '(859) 535-3975'
         },
         {
           id: '51601aaa-4e8f-4a9a-8964-84a684c0c43a',
-          fullname: 'Pena',
+          fullname: 'Michael de Santa',
           email: 'douglasprice@calcula.com',
           employeeRole: 'Manager',
           phoneNumber: '(959) 483-3327'
         },
         {
           id: '64dc37a5-3e28-4dac-9af7-2ae06f697356',
-          fullname: 'Levine',
+          fullname: 'Trevor Phillips',
           email: 'hilldyer@cincyr.com',
           employeeRole: 'Manager',
           phoneNumber: '(908) 430-3932'
         },
         {
           id: 'be394658-eb7a-422d-8495-884b15861613',
-          fullname: 'Hampton',
+          fullname: 'Franklin Clinton',
           email: 'dodsonchang@menbrain.com',
           employeeRole: 'Manager',
           phoneNumber: '(894) 515-2053'
