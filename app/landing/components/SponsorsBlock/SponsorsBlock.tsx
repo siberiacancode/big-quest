dd3ccdb7d4527d303979c71cec9a2dedd9c26ad6
Volkov Dmitriy <vdy0555@gmail.com>
2024-03-16 16:51:42 +0700

    feature/BQ-245 🚀 layout for sponsors block

--- app/landing/components/SponsorsBlock/SponsorsBlock.tsx
+++ app/landing/components/SponsorsBlock/SponsorsBlock.tsx
@@ -1,12 +1,41 @@
+import Image from 'next/image';
+
+import portrait1 from '@/assets/images/landing/sponsors/portrait1.png';
 import { I18nText } from '@/components/common';
 import { Typography } from '@/components/ui';
 
 export const SponsorsBlock = () => {
   return (
-    <div>
-      <Typography variant='h2'>
+    <div className='flex flex-col items-center px-10 pb-20 pt-14 mdx:pb-14 mdx:pt-8'>
+      <Typography variant='h1' tag='h2' className='mdx:text-[25px]'>
         <I18nText path='landing.sponsors.title' />
       </Typography>
+      <div className='mt-16 grid grid-cols-4 gap-12 mdx:mt-7 mdx:grid-cols-3 mdx:gap-7 xsx:grid-cols-2'>
+        <div>
+          <Image src={portrait1} alt='sponsor' />
+        </div>
+        <div>
+          <Image src={portrait1} alt='sponsor' />
+        </div>
+        <div>
+          <Image src={portrait1} alt='sponsor' />
+        </div>
+        <div>
+          <Image src={portrait1} alt='sponsor' />
+        </div>
+        <div>
+          <Image src={portrait1} alt='sponsor' />
+        </div>
+        <div>
+          <Image src={portrait1} alt='sponsor' />
+        </div>
+        <div>
+          <Image src={portrait1} alt='sponsor' />
+        </div>
+        <div>
+          <Image src={portrait1} alt='sponsor' />
+        </div>
+      </div>
     </div>
   );
 };
--- app/landing/components/StatisticsBlock/StatisticsBlock.tsx
+++ app/landing/components/StatisticsBlock/StatisticsBlock.tsx
@@ -14,26 +14,26 @@ export const StatisticsBlock = ({
 }: StatisticsBlockProps) => (
   <div className='flex w-full flex-wrap items-center justify-around bg-taiga px-5 py-7'>
     <div className='basis-1/3 px-3 py-3 text-center'>
-      <Typography tag='p' className='text-[40px] font-bold text-white'>
+      <Typography tag='p' className='text-[40px] font-bold text-white mdx:text-[32px]'>
         {participantsAmount}
       </Typography>
-      <Typography className='whitespace-nowrap text-[24px] text-white'>
+      <Typography className='whitespace-nowrap text-[24px] text-white mdx:text-[18px]'>
         <I18nText path='landing.statistics.participants' />
       </Typography>
     </div>
     <div className='basis-1/3 px-3 py-3 text-center'>
-      <Typography tag='p' className='text-[40px] font-bold text-white'>
+      <Typography tag='p' className='text-[40px] font-bold text-white mdx:text-[32px]'>
         {activitiesAmount}
       </Typography>
-      <Typography className='whitespace-nowrap text-[24px] text-white'>
+      <Typography className='whitespace-nowrap text-[24px] text-white mdx:text-[18px]'>
         <I18nText path='landing.statistics.activities' values={{ br: <br /> }} />
       </Typography>
     </div>
     <div className='basis-1/3 px-3 py-3 text-center'>
-      <Typography tag='p' className='text-[40px] font-bold text-white'>
+      <Typography tag='p' className='text-[40px] font-bold text-white mdx:text-[32px]'>
         {awardsAmount}
       </Typography>
-      <Typography className='whitespace-nowrap text-[24px] text-white'>
+      <Typography className='whitespace-nowrap text-[24px] text-white mdx:text-[18px]'>
         <I18nText path='landing.statistics.awards' />
       </Typography>
     </div>
