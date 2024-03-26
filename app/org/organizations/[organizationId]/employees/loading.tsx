b2a57673c769c9d893faa30073a7666d8b559e99
debabin <dmitriybabinup@gmail.com>
2024-03-27 00:50:56 +0700

    feature/BQ-251 🧊 fix loading page grid

--- app/org/organizations/[organizationId]/employees/loading.tsx
+++ app/org/organizations/[organizationId]/employees/loading.tsx
@@ -8,7 +8,7 @@ const EmployeeLoading = () => (
       <Skeleton className='h-10 w-36 bg-background' />
     </div>
 
-    <div className='justify-center gap-5 2xlx:grid-cols-2 xlx:flex xlx:flex-wrap 2xl:grid-cols-3 xl:grid'>
+    <div className='grid gap-3 4xlx:grid-cols-4 3xlx:grid-cols-3 2xlx:grid-cols-2 mdx:grid-cols-1'>
       {Array(5)
         .fill({})
         .map((_, index) => (
