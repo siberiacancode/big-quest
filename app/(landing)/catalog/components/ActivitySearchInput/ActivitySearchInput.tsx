07db140cda68498e3871cafbe8129c84068eaf82
venmak.li <kamnevin@gmail.com>
2024-04-11 12:54:25 +0700

    feature/BQ-260 🌰 added a button to cancel the search

--- app/(landing)/catalog/components/ActivitySearchInput/ActivitySearchInput.tsx
+++ app/(landing)/catalog/components/ActivitySearchInput/ActivitySearchInput.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { Search } from 'lucide-react';
+import { useState } from 'react';
+import { Search, X } from 'lucide-react';
 import { useDebounceCallback } from 'usehooks-ts';
 
-import { Input } from '@/components/ui';
+import { Button, Input } from '@/components/ui';
 import { useSearchParams } from '@/utils/hooks';
 
 const SEARCH_INPUT_DELAY = 500;
@@ -12,9 +13,24 @@ const ActivitySearchInput = () => {
   const { searchParams, setSearchParam } = useSearchParams();
   const activitySearchQueryText = searchParams.get('search');
 
-  const onActivitySearch = useDebounceCallback((text) => {
+  const [isInputEmpty, setIsInputEmpty] = useState(!activitySearchQueryText);
+  const [isInputText, setIsIputText] = useState(activitySearchQueryText ?? '');
+
+  const debounceSearch = useDebounceCallback((text) => {
     setSearchParam('search', text);
   }, SEARCH_INPUT_DELAY);
+
+  const onActivitySearch = (text) => {
+    setIsInputEmpty(text === '');
+    setIsIputText(text);
+    debounceSearch(text);
+  };
+
+  const onClear = () => {
+    setIsIputText('');
+    setIsInputEmpty(true);
+    setSearchParam('search', '');
+  };
 
   return (
     <div className='relative h-10 w-[293px] smx:mb-6'>
@@ -23,10 +39,20 @@ const ActivitySearchInput = () => {
       </div>
       <Input
         placeholder='Поиск актвности'
-        defaultValue={activitySearchQueryText ?? ''}
+        value={isInputText}
         onChange={(event) => onActivitySearch(event.target.value)}
         className='block h-10 rounded-lg border px-3 py-2 pl-10 text-sm leading-6 outline-none focus-visible:ring-0'
       />
+      {!isInputEmpty && (
+        <Button
+          variant='outline'
+          size='icon'
+          className='absolute inset-y-[11px] right-0 mr-3 size-[18px] rounded-full border-none bg-taiga hover:bg-taiga'
+          onClick={onClear}
+        >
+          <X strokeWidth={2} absoluteStrokeWidth size={8} className=' text-white' />
+        </Button>
+      )}
     </div>
   );
 };
