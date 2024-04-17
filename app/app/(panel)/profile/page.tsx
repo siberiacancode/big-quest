8e93b43ebd8ceca5a741d55906277740704e0121
Volkov Dmitrij <Dmitr.Volkov@cft.ru>
2024-04-17 17:56:21 +0700

    feature/BQ-269 🚀 start profile page

--- app/app/(panel)/profile/page.tsx
+++ app/app/(panel)/profile/page.tsx
@@ -1,3 +1,17 @@
-const AppProfilePage = () => <div />;
+import { SettingsIcon } from 'lucide-react';
+
+import { I18nText } from '@/components/common';
+import { Typography } from '@/components/ui';
+
+const AppProfilePage = () => (
+  <div>
+    <div className='flex items-center justify-between'>
+      <Typography>
+        <I18nText path='app.profile.title' />
+      </Typography>
+      <SettingsIcon />
+    </div>
+  </div>
+);
 
 export default AppProfilePage;
