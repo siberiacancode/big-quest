15bc1db106fced2aa29c96ccc5e66ecf444406a6
debabin <dmitriybabinup@gmail.com>
2024-04-18 22:20:34 +0700

    technical/api-type-generate 🧊 update package for api

--- src/utils/api/requests/auth/login/email/index.ts
+++ src/utils/api/requests/auth/login/email/index.ts
@@ -1,17 +1,8 @@
-import type {
-  BadRequestException,
-  LoginEmailDto,
-  LoginResponse,
-  UnauthorizedException
-} from '@/api-types';
+import type { LoginEmailDto, LoginResponse } from '@/api-types';
 import { api } from '@/utils/api/instance';
 
 export type PostAuthLoginEmailParams = LoginEmailDto;
 export type PostAuthLoginEmailRequestConfig = RequestConfig<PostAuthLoginEmailParams>;
 
 export const postAuthLoginEmail = async ({ params, config }: PostAuthLoginEmailRequestConfig) =>
-  api.post<LoginResponse | BadRequestException | UnauthorizedException>(
-    'auth/login/email',
-    params,
-    config
-  );
+  api.post<LoginResponse>('auth/login/email', params, config);
