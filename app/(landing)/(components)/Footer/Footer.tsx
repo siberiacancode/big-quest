46bfd93beabeed2b18b30fcd12c8cdccc674e61b
loyal_goose <polina.alina.2015@mail.ru>
2024-04-22 03:35:41 +0700

    feature/BQ-276 🐢 fix leave request and footer sections styles

--- app/(landing)/(components)/Footer/Footer.tsx
+++ app/(landing)/(components)/Footer/Footer.tsx
@@ -14,8 +14,8 @@ interface FooterProps {
 
 export const Footer = ({ cityId }: FooterProps) => (
   <footer className='bg-taiga text-white '>
-    <div className='container flex flex-wrap gap-28 py-20 lgx:justify-around'>
-      <div className='mr-[83px] flex flex-shrink-0 flex-col'>
+    <div className='container flex justify-between gap-28 py-20 xlx:gap-12 mdx:gap-8 2smx:flex-wrap 2smx:justify-around'>
+      <div className='flex flex-col xsx:w-full xsx:items-center md:mr-[83px]'>
         <Logo href={ROUTES.LANDING.CITY(cityId)} fill='white' />
         <div className='mt-14 flex gap-3'>
           {Object.entries(SOCIAL_LINKS[cityId.toUpperCase()]).map(
@@ -31,7 +31,7 @@ export const Footer = ({ cityId }: FooterProps) => (
           )}
         </div>
       </div>
-      <div>
+      <div className='xsx:flex xsx:w-full xsx:flex-col xsx:items-center'>
         <Typography tag='p' className='text-xl font-medium text-white'>
           <I18nText path='landing.footer.navigation.title' />
         </Typography>
@@ -80,8 +80,8 @@ export const Footer = ({ cityId }: FooterProps) => (
           </ul>
         </nav>
       </div>
-      <div className='lg:max-w-[337px]'>
-        <Typography tag='p' className='text-xl font-medium text-white'>
+      <div className='2smx: 2lgx:max-w-[250px] xsx:w-full xsx:items-center 2lg:max-w-[337px]'>
+        <Typography tag='p' className='text-xl font-medium text-white xsx:text-center'>
           <I18nText path='landing.footer.addresses' />
         </Typography>
         <nav className='mt-4'>
@@ -90,13 +90,13 @@ export const Footer = ({ cityId }: FooterProps) => (
           </ul>
         </nav>
       </div>
-      <div className='flex-grow text-end'>
+      <div className='text-end xsx:h-fit xsx:text-center 2sm:flex-grow'>
         <Link href={ROUTES.ORG.AUTH} className='text-white hover:underline'>
           <I18nText path='button.organizationsEntrance' />
         </Link>
       </div>
     </div>
-    <div className='container border-t border-solid border-white py-[25px] lgx:mt-20 mdx:mt-10'>
+    <div className='container border-t border-solid border-white py-[25px]'>
       <Typography variant='body4' className='text-center text-white'>
         <FooterCopyrightText />
       </Typography>
