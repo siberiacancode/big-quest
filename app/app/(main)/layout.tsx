c17f94cf480a37239a324ed62e14b7eb28103fbb
debabin <dmitriybabinup@gmail.com>
2024-04-21 17:02:38 +0700

    feature/BQ-269 🧊 rewrite styles

--- app/app/(main)/layout.tsx
+++ app/app/(main)/layout.tsx
@@ -1,15 +1,14 @@
-import { AppNavigation } from './(components)/AppNavigation/AppNavigation';
+import { AppBottomNavigation } from './(components)/AppBottomNavigation/AppBottomNavigation';
 
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
 const AppLayout = ({ children }: AppLayoutProps) => (
-  <div className='h-screen overflow-y-hidden'>
+  <div className='h-screen'>
     <div className='h-full overflow-y-auto pb-[100px] xxs:pb-[110px]'>{children}</div>
-    <div className='absolute bottom-0 left-0 right-0 z-10 h-[70px] xxs:h-[80px]'>
-      <AppNavigation />
-    </div>
+
+    <AppBottomNavigation />
   </div>
 );
 
