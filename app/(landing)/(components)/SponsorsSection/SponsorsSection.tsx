cdd8090765db876b252832a23dd70039d032d73b
loyal_goose <polina.alina.2015@mail.ru>
2024-04-22 08:10:15 +0700

    feature/BQ-276 🐢 fix landing sponsors style

--- app/(landing)/(components)/SponsorsSection/SponsorsSection.tsx
+++ app/(landing)/(components)/SponsorsSection/SponsorsSection.tsx
@@ -1,40 +1,47 @@
 import Image from 'next/image';
 
-import portrait1 from '@/assets/images/landing/sponsors/portrait1.png';
 import { I18nText } from '@/components/common';
+import { RegisterOrganizationDialog } from '@/components/dialogs';
 import { Typography } from '@/components/ui';
 
-export const SponsorsSection = () => (
-  <section id='sponsors' className='container mt-28 flex flex-col items-center'>
-    <Typography variant='h1' tag='h2' className='mdx:text-3xl xsx:text-[21px]'>
-      <I18nText path='landing.sponsors.title' />
-    </Typography>
+import { sponsors } from './constants/sponsors';
 
-    <div className='mt-16 grid grid-cols-2 gap-6 md:grid-cols-4'>
-      <div>
-        <Image src={portrait1} alt='sponsor' />
+export const SponsorsSection = () => (
+  <section id='sponsors' className='relative w-full bg-muted pb-[104px] pt-[72px]'>
+    <div className='z-4 container mx-4 flex w-fit flex-col items-center rounded-[30px] bg-white px-[111px] py-[72px] 2mdx:px-[80px] 2smx:px-[24px]'>
+      <div className='flex flex-col items-center'>
+        <Typography variant='h1' tag='h2' className='mb-8 mdx:text-3xl xsx:text-[21px]'>
+          <I18nText path='landing.sponsors.title' />
+        </Typography>
+        <Typography variant='body1' tag='p' className='text-center xsx:text-[21px]'>
+          <I18nText path='landing.sponsors.description' />{' '}
+        </Typography>
+        <RegisterOrganizationDialog
+          trigger={
+            <Typography
+              variant='body1'
+              tag='p'
+              className='text-center font-medium lowercase underline xsx:text-[21px]'
+            >
+              <I18nText path='landing.leaveRequest.subtitle' />
+            </Typography>
+          }
+        />
       </div>
-      <div>
-        <Image src={portrait1} alt='sponsor' />
-      </div>
-      <div>
-        <Image src={portrait1} alt='sponsor' />
-      </div>
-      <div>
-        <Image src={portrait1} alt='sponsor' />
-      </div>
-      <div>
-        <Image src={portrait1} alt='sponsor' />
-      </div>
-      <div>
-        <Image src={portrait1} alt='sponsor' />
-      </div>
-      <div>
-        <Image src={portrait1} alt='sponsor' />
-      </div>
-      <div>
-        <Image src={portrait1} alt='sponsor' />
+
+      <div className='mt-[72px] grid grid-cols-3 gap-12 2xl:grid-cols-6'>
+        {sponsors.map((sponsor) => (
+          <div key={sponsor.alt}>
+            <Image
+              src={sponsor.image}
+              alt={sponsor.alt}
+              className='size-[143px] 2mdx:size-[124px] 2smx:size-[80px]'
+            />
+          </div>
+        ))}
       </div>
     </div>
+
+    <div className='z-1 container absolute -left-4 top-5 rounded-[30px] border border-dashed border-taiga px-[111px]  py-[72px] 2mdx:px-[80px] 2smx:px-[24px]' />
   </section>
 );
---
+++ app/(landing)/(components)/SponsorsSection/constants/sponsors.ts
@@ -0,0 +1,33 @@
+import sponsor1 from '@/assets/images/landing/sponsors/sponsor1.png';
+import sponsor2 from '@/assets/images/landing/sponsors/sponsor2.png';
+import sponsor3 from '@/assets/images/landing/sponsors/sponsor3.png';
+import sponsor4 from '@/assets/images/landing/sponsors/sponsor4.png';
+import sponsor5 from '@/assets/images/landing/sponsors/sponsor5.png';
+import sponsor6 from '@/assets/images/landing/sponsors/sponsor6.png';
+
+export const sponsors = [
+  {
+    image: sponsor1,
+    alt: '2GIS'
+  },
+  {
+    image: sponsor2,
+    alt: '?'
+  },
+  {
+    image: sponsor3,
+    alt: 'Vk'
+  },
+  {
+    image: sponsor4,
+    alt: '?'
+  },
+  {
+    image: sponsor5,
+    alt: 'Gazprom'
+  },
+  {
+    image: sponsor6,
+    alt: '?'
+  }
+];
