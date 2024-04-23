7715694b1874cd903fc699771c2c289e8895f65e
loyal_goose <polina.alina.2015@mail.ru>
2024-04-23 20:52:31 +0700

    feature/BQ-276 🐢 fix sponsor dashed border styles

--- app/(landing)/(components)/SponsorsSection/SponsorsSection.tsx
+++ app/(landing)/(components)/SponsorsSection/SponsorsSection.tsx
@@ -11,36 +11,39 @@ export const SponsorsSection = () => (
     id='sponsors'
     className='relative w-full bg-muted pb-10 pt-[72px] xlx:pb-5 xl:pb-[104px]'
   >
-    <div className='container relative z-10 flex w-fit flex-col rounded-[30px] bg-white px-[111px] py-[72px] 2mdx:px-[80px] mdx:mx-4 2smx:px-[24px]'>
-      <div className='flex flex-col items-center'>
-        <Typography variant='h1' tag='h2' className='mb-8 mdx:text-3xl xsx:text-[21px]'>
-          <I18nText path='landing.sponsors.title' />
-        </Typography>
-        <Typography variant='body1' tag='p' className='text-center xsx:text-[21px]'>
-          <I18nText path='landing.sponsors.description' />
-        </Typography>
-        <RegisterOrganizationDialog
-          trigger={
-            <Typography
-              variant='body1'
-              tag='p'
-              className='text-center font-medium lowercase underline xsx:text-[21px]'
-            >
-              <I18nText path='landing.leaveRequest.subtitle' />
-            </Typography>
-          }
-        />
-      </div>
-      <div className='mx-auto mt-[72px] grid w-fit grid-cols-6 gap-5 xlx:mt-10 2mdx:grid-cols-3 xl:grid-cols-6 xl:gap-12'>
-        {sponsors.map((sponsor) => (
-          <div key={sponsor.alt}>
-            <Image
-              src={sponsor.image}
-              alt={sponsor.alt}
-              className='size-[143px] 2xlx:size-[120px] 2lgx:size-[100px] 2mdx:size-[124px] 2smx:size-[100px] xsx:size-[80px]'
-            />
-          </div>
-        ))}
+    <div className='container relative w-max mdx:max-w-full'>
+      <div className='z-3 absolute -left-8 top-10 h-full w-full rounded-[30px] border-2 border-dashed border-taiga-light xlx:-left-1 xlx:w-[94%] 2mdx:w-[93%] mdx:left-3 mdx:top-6 mdx:w-[88%]' />
+      <div className='relative z-10 flex w-fit flex-col rounded-[30px] bg-white px-[111px] py-[72px] 2mdx:px-[80px] mdx:mx-2 2smx:px-[24px]'>
+        <div className='flex flex-col items-center'>
+          <Typography variant='h1' tag='h2' className='mb-8 mdx:text-3xl xsx:text-[21px]'>
+            <I18nText path='landing.sponsors.title' />
+          </Typography>
+          <Typography variant='body1' tag='p' className='text-center xsx:text-[21px]'>
+            <I18nText path='landing.sponsors.description' />
+          </Typography>
+          <RegisterOrganizationDialog
+            trigger={
+              <Typography
+                variant='body1'
+                tag='p'
+                className='text-center font-medium lowercase underline xsx:text-[21px]'
+              >
+                <I18nText path='landing.leaveRequest.subtitle' />
+              </Typography>
+            }
+          />
+        </div>
+        <div className='mx-auto mt-[72px] grid w-fit grid-cols-6 gap-5 xlx:mt-10 2mdx:grid-cols-3 xl:grid-cols-6 xl:gap-12'>
+          {sponsors.map((sponsor) => (
+            <div key={sponsor.alt}>
+              <Image
+                src={sponsor.image}
+                alt={sponsor.alt}
+                className='size-[143px] 2xlx:size-[120px] 2lgx:size-[100px] 2mdx:size-[124px] 2smx:size-[100px] xsx:size-[80px]'
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   </section>
