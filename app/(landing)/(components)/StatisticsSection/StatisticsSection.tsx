0f7125e06b3fae496931c6a2b9950e89b587e3a6
Polina <polina.alina.2015@mail.ru>
2024-05-03 23:21:56 +0700

    feature/BQ-276 🐢 fix statistics style

--- app/(landing)/(components)/StatisticsSection/StatisticsSection.tsx
+++ app/(landing)/(components)/StatisticsSection/StatisticsSection.tsx
@@ -6,66 +6,33 @@ import { Typography } from '@/components/ui';
 
 export const StatisticsSection = () => (
   <section className='w-full bg-muted py-12'>
-    <div className='container grid w-full max-w-[1320px] grid-cols-1 justify-center gap-10 px-0 xlx:gap-6 2lgx:px-0 3mdx:gap-4 3mdx:px-4 3smx:px-4 xs:grid-cols-2 md:grid md:grid-cols-3 md:flex-nowrap 2xl:px-0 '>
-      <div className='relative flex h-[265px] w-full flex-col justify-between overflow-hidden rounded-[30px] bg-white px-8 py-7 3mdx:h-[187px] mdx:rounded-[16px] 2xsx:w-full md:max-w-full 2lg:max-w-[413px]'>
-        <Typography
-          tag='h3'
-          variant='h3'
-          className='text-5xl font-bold text-taiga-light 3mdx:text-[40px]'
-        >
+    <div className='container grid w-full max-w-[1320px] grid-cols-1 justify-center gap-10 xlx:gap-6 3mdx:gap-4 xs:grid-cols-2 md:grid md:grid-cols-3'>
+      <div className='relative flex h-fit w-full flex-col justify-between gap-8 overflow-hidden rounded-[30px] bg-white px-8 py-7 mdx:rounded-[16px] lg:gap-20'>
+        <Typography tag='h3' variant='h3' className='text-5xl font-semibold text-taiga-light'>
           3000
         </Typography>
-        <Typography
-          variant='h4'
-          className='max-w-[154px] font-normal text-gray-two 3mdx:text-xl 3smx:max-w-none xsx:max-w-[154px] xxsx:text-lg'
-        >
+        <Typography variant='h4' className='max-w-[161px] font-normal text-gray-two'>
           <I18nText path='landing.statistics.participants' />
         </Typography>
-        <Image
-          src={tree}
-          alt='Tree'
-          className='absolute -right-1 top-[25%] 3mdx:h-[88px] 3mdx:w-[37px] 3md:-right-5'
-        />
+        <Image src={tree} alt='Tree' className='absolute -right-4 h-[142px]' />
       </div>
-      <div className='relative flex h-[265px] w-full flex-col justify-between rounded-[30px] bg-white px-8 py-7 3mdx:h-[187px] mdx:rounded-[16px] 2xsx:w-full md:max-w-full 2lg:max-w-[413px]'>
-        <Typography
-          tag='h3'
-          variant='h3'
-          className='text-5xl font-bold text-taiga-light 3mdx:text-[40px]'
-        >
+      <div className='relative flex h-fit w-full flex-col justify-between gap-8 overflow-hidden rounded-[30px] bg-white px-8 py-7 mdx:rounded-[16px] lg:gap-20'>
+        <Typography tag='h3' variant='h3' className='text-5xl font-semibold text-taiga-light'>
           200
         </Typography>
-        <Typography
-          variant='h4'
-          className='max-w-[154px] font-normal text-gray-two 3mdx:text-xl 3smx:max-w-none xsx:max-w-[154px] xxsx:text-lg'
-        >
+        <Typography variant='h4' className='max-w-[161px] font-normal text-gray-two'>
           <I18nText path='landing.statistics.activities' />
         </Typography>
-        <Image
-          src={tree}
-          alt='Tree'
-          className='absolute -right-1 top-[25%] 3mdx:h-[88px] 3mdx:w-[37px] 3md:-right-5'
-        />
+        <Image src={tree} alt='Tree' className='absolute -right-4 h-[142px]' />
       </div>
-      <div className='relative col-span-2 flex h-[265px] w-full flex-col justify-between rounded-[30px] bg-white px-8 py-7 3mdx:h-[187px] 2mdx:gap-4 mdx:col-span-2 mdx:rounded-[16px] xsx:col-span-1 md:col-span-1 md:w-full'>
-        <Typography
-          tag='h3'
-          variant='h3'
-          className='text-5xl font-bold text-taiga-light 3mdx:text-[40px]'
-        >
+      <div className='relative col-span-2 flex h-fit w-full flex-col justify-between gap-8 overflow-hidden rounded-[30px] bg-white px-8 py-7 mdx:col-span-2 mdx:rounded-[16px] xsx:col-span-1 md:col-span-1 md:w-full lg:gap-20'>
+        <Typography tag='h3' variant='h3' className='text-5xl font-semibold text-taiga-light'>
           1150
         </Typography>
-        <Typography
-          variant='h4'
-          className='max-w-[161px]  font-normal text-gray-two 2xlx:max-w-none 3mdx:text-xl xsx:max-w-[154px]'
-        >
+        <Typography variant='h4' className='max-w-[161px] font-normal text-gray-two'>
           <I18nText path='landing.statistics.awards' />
         </Typography>
-        <Image
-          src={tree}
-          alt='Tree'
-          className='absolute -right-1 top-[25%] 3mdx:h-[88px] 3mdx:w-[37px] 3md:-right-5'
-        />
+        <Image src={tree} alt='Tree' className='absolute -right-4 h-[142px]' />
       </div>
     </div>
   </section>
