d442b7cdce1d7ba406ff5d622131ec1c385bd1ba
Daniil Khakhulin <dan.praeco@gmail.com>
2024-05-16 19:55:26 +0700

    feature/BQ-277 🌊 added button variant with link

--- app/app/profile/edit/page.tsx
+++ app/app/profile/edit/page.tsx
@@ -2,7 +2,8 @@ import { ChevronLeftIcon } from 'lucide-react';
 import Link from 'next/link';
 
 import { I18nText } from '@/components/common';
-import { Typography } from '@/components/ui';
+import { buttonVariants, Typography } from '@/components/ui';
+import { cn } from '@/lib/utils';
 import { getUserProfile } from '@/utils/api';
 import { ROUTES } from '@/utils/constants';
 
@@ -14,7 +15,10 @@ const ProfileEditPage = async () => {
   return (
     <div className='px-4 pt-8'>
       <div className='flex text-nowrap'>
-        <Link href={ROUTES.APP.PROFILE} className='absolute'>
+        <Link
+          href={ROUTES.APP.ACTIVITIES}
+          className={cn(buttonVariants({ variant: 'link' }), 'absolute')}
+        >
           <ChevronLeftIcon size={32} className='rounded-md border' />
         </Link>
         <Typography variant='h6' className='flex-grow text-center font-semibold'>
