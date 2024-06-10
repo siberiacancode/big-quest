b7c914e18d89dd747df81cbac1fd647ca39616d0
Daniil Khakhulin <dan.praeco@gmail.com>
2024-06-11 00:28:02 +0700

    technical/BQ-290 🌊 comment delete

--- app/org/(panel)/organizations/[organizationId]/addresses/(components)/ActionAddressForm/helpers/convertLocalitiesToComboboxItems.ts
+++ app/org/(panel)/organizations/[organizationId]/addresses/(components)/ActionAddressForm/helpers/convertLocalitiesToComboboxItems.ts
@@ -1,9 +1,7 @@
 import type { AddressResponseDto } from '@/api-types';
 
 export const convertLocalitiesToComboboxItems = (addresses: AddressResponseDto[]) =>
-  addresses
-    // .filter((address) => address.street && address.house)
-    .map((address) => ({
-      label: address.value,
-      value: address
-    }));
+  addresses.map((address) => ({
+    label: address.value,
+    value: address
+  }));
