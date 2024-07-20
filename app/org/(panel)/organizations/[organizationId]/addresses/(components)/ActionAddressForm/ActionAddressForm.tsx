5203c7adb19ef68a75a1e870d3459150ec463f13
Daniil Khakhulin <dan.praeco@gmail.com>
2024-07-21 00:08:10 +0700

    technical/addresses-update 🌊 locale fix

--- app/org/(panel)/organizations/[organizationId]/addresses/(components)/ActionAddressForm/ActionAddressForm.tsx
+++ app/org/(panel)/organizations/[organizationId]/addresses/(components)/ActionAddressForm/ActionAddressForm.tsx
@@ -85,7 +85,7 @@ export const ActionAddressForm = <ActionType extends AddressActionType>({
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>
-                      <I18nText path='field.address.label' />
+                      <I18nText path='field.locality.label' />
                     </FormLabel>
                     <AddressCombobox
                       value={field.value as ComboBoxOption<AddressResponseDto>}
